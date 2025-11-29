1efdd48faa375174be68a5f816900ae91426e1ed
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-11-29 21:04:55 +0200

    add pagination

--- railway_service/settings.py
+++ railway_service/settings.py
@@ -152,7 +152,11 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_RATES": {
         "anon": "100/day",
         "user": "1000/day"
-    }
+    },
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.LimitOffsetPagination",
+    ),
+    "PAGE_SIZE": 100,
 }
 
 SIMPLE_JWT = {
